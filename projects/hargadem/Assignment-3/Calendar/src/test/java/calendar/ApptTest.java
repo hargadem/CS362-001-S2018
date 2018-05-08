edf182dc9e52d41d818330c0fc50a9c5f0fde4ed
Maire Hargaden <hargadem@oregonstate.edu>
2018-05-07 17:27:38 -0700

    added test cases to cover mutations

---
+++ projects/hargadem/Assignment-3/Calendar/src/test/java/calendar/ApptTest.java
@@ -0,0 +1,68 @@
+/** A JUnit test class to test the class ApptTest. */
+
+package calendar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import calendar.Appt;
+import calendar.CalendarUtil;
+public class ApptTest  {
+    
+    
+    int startHour=16;
+    int startMinute=15;
+    int startDay=7;
+    int startMonth=5;
+    int startYear=2018;
+    String title="Party";
+    String description="My birthday party.";
+    String email="email@email.com";
+    //Construct a new Appointment object with the initial data
+    Appt appt = new Appt(startHour,
+                         startMinute ,
+                         startDay ,
+                         startMonth ,
+                         startYear ,
+                         title,
+                         description,
+                         email);
+
+  @Test(timeout = 4000)
+  public void test00()  throws Throwable  {
+      Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+      String string0 = appt0.toString();
+      assertEquals(2, appt0.getRecurBy());
+      assertFalse(appt0.isRecurring());
+      assertEquals("\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
+      assertEquals(0, appt0.getRecurIncrement());
+      appt0.setValid();
+  }
+
+	@Test(timeout = 4000)
+	public void test01()  throws Throwable 
+	{
+		Appt appt1 = new Appt(22,10,2018,"Work","go to work","myemail@gmail.com");
+
+		assertEquals(0, appt1.getRecurNumber());
+		assertTrue(appt1.isOn(22, 10, 2018));
+		assertTrue(appt1.getValid());
+		assertEquals("myemail@gmail.com",appt1.getEmailAddress());
+		assertEquals("go to work", appt1.getDescription());
+		assertEquals("Work", appt1.getTitle());
+		assertEquals(2018, appt1.getStartYear());
+		assertEquals(10, appt1.getStartMonth());
+	}
+    
+    @Test
+    public void test07()    throws Throwable {
+        appt.setStartMonth(13);
+        appt.setStartMonth(12);
+        appt.setStartHour(11);
+        assertFalse(appt.toString().contains("pm"));
+        assertFalse(appt.toString().contains("-1"));
+    }
+}
+    
+    
+
+
---
+++ projects/hargadem/Assignment-3/Calendar/src/test/java/calendar/DataHandlerTest.java
@@ -0,0 +1,69 @@
+
+/** A JUnit test class to test the class DataHandler. */
+
+
+package calendar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import calendar.Appt;
+import calendar.CalDay;
+import calendar.DataHandler;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.*;
+
+
+public class DataHandlerTest{
+    
+  @Test(timeout = 4000)
+  public void test00()  throws Throwable  {
+	DataHandler handle0 = new DataHandler();
+	GregorianCalendar day1 = new GregorianCalendar();
+	GregorianCalendar day2 = new GregorianCalendar();
+	day1.set(GregorianCalendar.MONTH,-1);
+	assertNotNull(handle0.getApptRange(day1, day2));
+  }
+    
+  @Test(timeout = 4000)
+  public void test01()  throws Throwable  {
+	DataHandler handle1 = new DataHandler("file1.xml");
+	Appt invalidappt = new Appt(-1,1,2018,"Invalid Appt","invalid","myemail");
+	invalidappt.setValid();
+	assertFalse(handle1.saveAppt(invalidappt));
+	assertFalse(handle1.deleteAppt(invalidappt));
+  }
+
+  @Test(timeout = 4000)
+  public void test02() throws Throwable	{
+	Appt newappt = new Appt(-1,-1,-1,"New Appt","test appt","myemail");
+	DataHandler handle2 = new DataHandler("file2.xml", false);
+      DataHandler handle3 = new DataHandler("file3.xml", true);
+	assertTrue(handle2.save());
+	assertTrue(handle2.saveAppt(newappt));
+      assertTrue(handle2.saveAppt(newappt));
+	assertTrue(handle2.deleteAppt(newappt));
+      assertFalse(handle3.deleteAppt(newappt));
+     newappt.setValid();
+      assertFalse(handle2.saveAppt(newappt));
+      assertFalse(handle3.saveAppt(newappt));
+      
+  }
+  
+    @Test
+    public void test03()    throws Throwable    {
+        DataHandler handle = new DataHandler();
+        LinkedList<Appt> appts = new LinkedList<Appt>();
+        appts.add(new Appt(5, 30, 1, 5, 2018, "Birthday", "Birthday Party","email"));
+        appts.add(new Appt(4, 30, 1, 4, 2018, "Work", "My Shift","email"));
+        
+        List<CalDay> calDayTest;
+        calDayTest = handle.getApptRange(new GregorianCalendar(2017, 4, 28, 14, 30, 0),
+                                            new GregorianCalendar(2017, 4, 31, 19, 30, 0));
+        
+        assertTrue(calDayTest.get(0).getSizeAppts() == 0);
+    }
+  
+ 
+}
